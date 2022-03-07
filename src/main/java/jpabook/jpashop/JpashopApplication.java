cd0f41452abf7b9660981f307e1ebd27cf75a7cb
hyh <qkdxo101@naver.com>
2022-03-07 23:21:06 +0900

    6강

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,10 +3,11 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//develop
 @SpringBootApplication
 public class JpashopApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
